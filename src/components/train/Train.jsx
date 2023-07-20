2f9d4a84a5a2d6aa4f7964e48ac38a93dac2d88e
Soboleva Natalia <nsoboleva1979@rambler.ru>
2023-07-20 12:01:02 +0300

    поезд.Начало

--- src/components/train/Train.jsx
+++ src/components/train/Train.jsx
@@ -3,16 +3,21 @@ import { ReactComponent as Loco } from './../../images/loco.svg';
 import s from './train.module.css';
 
 export const Train = ({ item }) => {
-  console.log(item.departure.train.name);
+  const data = item.departure;
+  console.log(data);
   return (
     <div className={s.container}>
       <div className={s.header}>
         <Loco />
         <p>{item.departure.train.name}</p>
-        <p></p>
+        <p>
+          {data.from.city.name} - {data.to.city.name}
+        </p>
       </div>
       <div className={s.main}></div>
-      <div className={s.footer}></div>
+      <div className={s.footer}>
+        <button>Выбрать места</button>
+      </div>
     </div>
   );
 };
