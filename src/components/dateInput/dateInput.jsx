36780d4d642c8fb05f2b722c18c14413205065bb
Андрей <andruxa21097@gmail.com>
2023-07-24 17:15:02 +0700

    Настройка фильтров

--- src/components/dateInput/dateInput.jsx
+++ src/components/dateInput/dateInput.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import s from './dateInput.module.css';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { setDate } from '../../storage/slices/trainSlice';
 
-const DateInput = ({name}) => {
+const DateInput = ({ name, getTrains = false }) => {
   const dispatch = useDispatch();
-  const today = new Date().toLocaleDateString('en-ca')
+  const { searchParams } = useSelector((s) => s.trains);
+  const today = new Date().toLocaleDateString('en-ca');
 
   const selectDate = (e) => {
-    dispatch(setDate({value: e.target.value, name}))
-  }
+    dispatch(setDate({ name, value: e.target.value }));
+    getTrains && dispatch(getTrains());
+  };
 
   return (
-    <input className={s['dateInput']} min={today} onChange={selectDate} type='date' name={name}  />
+    <input
+      className={s['dateInput']}
+      min={today}
+      onChange={selectDate}
+      type='date'
+      defaultValue={searchParams[name]}
+      name={name}
+    />
   );
-}
+};
 
 export default DateInput;
--- src/pages/ticketsPage/TicketsPage.jsx
+++ src/pages/ticketsPage/TicketsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getCoach, getTrains, setFilter } from '../../storage/slices/trainSlice';
+import { getCoach } from '../../storage/slices/trainSlice';
 import { RouteDetails } from '../../components/routeDetails/routeDetails';
 import { TicketsQuantity } from '../../components/ticketsQuantity/ticketsQuantity';
 import { Header } from '../../components/headerBlock/header/Header';
@@ -20,7 +20,6 @@ export const TicketsPage = () => {
     navigate('/stepthree');
   };
   const {
-    filters,
     currentRoute,
     departureCoach,
     departureCoachType,
@@ -34,12 +33,6 @@ export const TicketsPage = () => {
     dispatch(getCoach(currentRoute));
   }, [dispatch, currentRoute]);
 
-  // DEV тест переключателя фильтров
-  const handleFilter = (filter) => {
-    dispatch(setFilter(filter));
-    dispatch(getTrains());
-  };
-
   return (
     <div>
       <Header />
@@ -47,24 +40,6 @@ export const TicketsPage = () => {
       <div className={s.container}>
         <DetailsFilter />
         <div className={s.ticketsBlock}>
-          <div className={s['filters']}>
-            <div className={s['filter']}>
-              <button onClick={() => handleFilter('have_first_class')}>Люкс</button>
-              <span>{filters.have_first_class ? 'Да' : 'Нет'}</span>
-            </div>
-            <div className={s['filter']}>
-              <button onClick={() => handleFilter('have_second_class')}>Купе</button>
-              <span>{filters.have_second_class ? 'Да' : 'Нет'}</span>
-            </div>
-            <div className={s['filter']}>
-              <button onClick={() => handleFilter('have_third_class')}>Плацкарт</button>
-              <span>{filters.have_third_class ? 'Да' : 'Нет'}</span>
-            </div>
-            <div className={s['filter']}>
-              <button onClick={() => handleFilter('have_fourth_class')}>Сидячий</button>
-              <span>{filters.have_fourth_class ? 'Да' : 'Нет'}</span>
-            </div>
-          </div>
           <RouteDetails direction={'departure'} routeInfo={currentRoute.departure} />
           <TicketsQuantity />
           <CoachTypeSelect routeInfo={currentRoute.departure} direction={'departure'} />
@@ -77,14 +52,6 @@ export const TicketsPage = () => {
             ))}
           <hr />
           {!!departureFilteredCoach.length && !!departureCoachType && <Coach coach={departureFilteredCoach} />}
-          {/* {!!departureFilteredCoach.length &&
-            !!departureCoachType &&
-            departureFilteredCoach.map(({ coach }, i) => (
-              <div key={i}>
-                <p>{coach.name}</p>
-                <p>{coach.class_type}</p>
-              </div>
-            ))} */}
           {currentRoute.arrival && (
             <>
               <RouteDetails direction={'arrival'} routeInfo={currentRoute.arrival} />
