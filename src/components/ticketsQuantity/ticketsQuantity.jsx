4d335a40392f551791adb50b97218f525b8e6a57
Андрей <andruxa21097@gmail.com>
2023-08-15 09:48:35 +0700

    Функция расчета количества билетов по группам

--- src/components/ticketsQuantity/ticketsQuantity.jsx
+++ src/components/ticketsQuantity/ticketsQuantity.jsx
@@ -1,30 +1,105 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import s from './ticketsQuantity.module.css';
 
 export const TicketsQuantity = () => {
+  const maxAdultTicketsCount = 5;
+  const [quantityAdult, setQuantityAdult] = useState(0);
+  const [quantityChildren, setQuantityChildren] = useState(0);
+  const [maxQuantityChildren, setMaxQuantityChildren] = useState(0);
+  const [quantityChildrenNoPlace, setQuantityChildrenNoPlace] = useState(0);
+  const [maxQuantityChildrenNoPlace, setMaxQuantityChildrenNoPlace] = useState(0);
+
+  useEffect(() => {
+    if (quantityAdult * 2 - quantityChildrenNoPlace < quantityChildren) {
+      setQuantityChildren(quantityChildren > 1 ? quantityAdult * 2 - quantityChildrenNoPlace : 0);
+    }
+    if (quantityAdult >= quantityChildren && quantityAdult < quantityChildrenNoPlace) {
+      setQuantityChildrenNoPlace(quantityAdult);
+    } else if (quantityAdult < quantityChildren && quantityAdult < quantityChildrenNoPlace) {
+      setMaxQuantityChildrenNoPlace(
+        quantityChildrenNoPlace > 0 ? quantityAdult * 2 - quantityChildren : 0
+      );
+    }
+
+    setMaxQuantityChildren(quantityAdult * 2 - quantityChildrenNoPlace);
+
+    if (quantityAdult > quantityChildren) {
+      setMaxQuantityChildrenNoPlace(quantityAdult);
+    } else {
+      setMaxQuantityChildrenNoPlace(quantityAdult * 2 - quantityChildren);
+    }
+  }, [
+    quantityAdult,
+    quantityChildren,
+    quantityChildrenNoPlace,
+    maxQuantityChildren,
+    maxQuantityChildrenNoPlace,
+  ]);
+
   return (
     <div>
       <span className={s['title']}>Количество билетов</span>
-      <div className={s['container']}>
+      <div className={s['ticketsQuantityContainer']}>
         <div className={s['ticketSelector-container']}>
-          <select className={s['ticketSelector']} name='' id=''>
-            <option value='1'>Взрослых - 1</option>
-          </select>
-          <span>Можно добавить еще 3 пассажиров </span>
+          <label>
+            <div className={s['container']}>
+              <span className={s['prefix']}>Взрослых —</span>
+              <input
+                className={s['input']}
+                type='number'
+                min={0}
+                max={maxAdultTicketsCount}
+                value={quantityAdult}
+                onChange={(e) => setQuantityAdult(Number(e.target.value))}
+              />
+            </div>
+          </label>
+          {!!quantityAdult && quantityAdult !== 5 && (
+            <span>Можно добавить еще {maxAdultTicketsCount - quantityAdult} пассажиров</span>
+          )}
+          {quantityAdult === 5 && <span>Максимум 5 билетов за один заказ</span>}
         </div>
         <div className={s['ticketSelector-container']}>
-          <select className={s['ticketSelector']} name='' id=''>
-            <option value='1'>Детских - 1</option>
-          </select>
-          <span>
-            Можно добавить еще 3 детей до 10 лет.Свое место в вагоне, как у взрослых, но дешевле в
-            среднем на 50-65%
-          </span>
+          <label>
+            <div className={s['container']}>
+              <span className={s['prefix']}>Детских —</span>
+              <input
+                className={s['input']}
+                type='number'
+                min={0}
+                max={maxQuantityChildren}
+                value={quantityChildren}
+                onChange={(e) => setQuantityChildren(Number(e.target.value))}
+              />
+            </div>
+          </label>
+          {!!quantityAdult && quantityChildren !== maxQuantityChildren && (
+            <span>
+              Можно добавить еще {maxQuantityChildren - quantityChildren} детей до 10 лет.Свое место
+              в вагоне, как у взрослых, но дешевле в среднем на 50-65%
+            </span>
+          )}
         </div>
         <div className={s['ticketSelector-container']}>
-          <select className={s['ticketSelector']} name='' id=''>
-            <option value='1'>Детских «без места» - 1</option>
-          </select>
+          <label>
+            <div className={s['container']}>
+              <span className={s['prefix']}>Детских «без места» —</span>
+              <input
+                className={s['input']}
+                type='number'
+                min={0}
+                max={maxQuantityChildrenNoPlace}
+                value={quantityChildrenNoPlace}
+                onChange={(e) => setQuantityChildrenNoPlace(Number(e.target.value))}
+              />
+            </div>
+          </label>
+          {!!quantityAdult && quantityChildrenNoPlace !== maxQuantityChildrenNoPlace && (
+            <span>
+              Можно добавить еще {maxQuantityChildrenNoPlace - quantityChildrenNoPlace} детей до 5
+              лет без занятия отдельного места.
+            </span>
+          )}
         </div>
       </div>
     </div>
