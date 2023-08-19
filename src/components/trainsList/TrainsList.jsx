8642ac2f910d0eadd80fdf55b1f9087653a244cd
Андрей <andruxa21097@gmail.com>
2023-08-19 15:08:20 +0700

    Сортировка списка поездов

--- src/components/trainsList/TrainsList.jsx
+++ src/components/trainsList/TrainsList.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Train } from '../train/Train';
 import s from './trainList.module.css';
+import { useDispatch } from 'react-redux';
+import { getTrains, setFilter } from '../../storage/slices/trainSlice';
 
 export const TrainsList = ({ trains, total }) => {
+  const dispatch = useDispatch();
+
+  const handleFilter = (filter, e) => {
+    dispatch(setFilter({ filter, value: e.target.value }));
+    dispatch(getTrains());
+  };
+
   return (
     <div>
       <div className={s['total-admin']}>
@@ -10,17 +19,40 @@ export const TrainsList = ({ trains, total }) => {
         <div className={s['total-sort']}>
           <div className={s['total-select']}>
             <label htmlFor=''>сортировать по: </label>
-            <select name='' id=''>
-              <option value='времени'>времени</option>
-              <option value='стоимости'>стоимости</option>
-              <option value='длительности'>длительности</option>
+            <select onChange={(e) => handleFilter('sort', e)}>
+              <option value='date'>времени</option>
+              <option value=''>стоимости</option>
+              <option value='duration'>длительности</option>
             </select>
           </div>
-          <p className={s['total-show']}>
-            показывать по: <span className={s['active']}>5</span>
-            <span className={s['total']}>10</span>
-            <span className={s['total']}>20</span>
-          </p>
+          <div className={s['totalSelect']}>
+            <p className={s['total-show']}>показывать по:</p>
+            <input
+              onChange={(e) => handleFilter('limit', e)}
+              className={s['total']}
+              data_count='5'
+              type='radio'
+              name='countOnPage'
+              value={5}
+              defaultChecked={true}
+            />
+            <input
+              onChange={(e) => handleFilter('limit', e)}
+              className={s['total']}
+              data_count='10'
+              type='radio'
+              name='countOnPage'
+              value={10}
+            />
+            <input
+              onChange={(e) => handleFilter('limit', e)}
+              className={s['total']}
+              data_count='20'
+              type='radio'
+              name='countOnPage'
+              value={20}
+            />
+          </div>
         </div>
       </div>
       <div>
